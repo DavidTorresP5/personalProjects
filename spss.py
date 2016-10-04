aaebf87dd76329a0b69f03aaefcd6d8164889e45
David Torres Pascual <davidtorresp5@gmail.com>
2016-10-04 20:11:46 +0200

    Delete spss.py

--- spss.py
+++
@@ -1,9 +0,0 @@
-# pip install savReaderWriter
-
-import pandas as pd
-import savReaderWriter as spss
-
-url = 'url/file.sav'
-raw_data = spss.SavReader(url, returnHeader=True, rawMode=False)
- 
-df = pd.DataFrame(list(raw_data)) 
