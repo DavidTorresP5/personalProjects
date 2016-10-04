6a5c691413ac650a690e862b58e5bc6f04e3114e
David Torres Pascual <davidtorresp5@gmail.com>
2016-10-04 19:07:42 +0200

    Update spss.py

--- spss.py
+++ spss.py
@@ -1,3 +1,9 @@
+# pip install savReaderWriter
+
 import pandas as pd
 import savReaderWriter as spss
 
+url = 'url/file.sav'
+raw_data = spss.SavReader(url, returnHeader=True, rawMode=False)
+ 
+df = pd.DataFrame(list(raw_data)) 
